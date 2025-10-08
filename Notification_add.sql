ca8bad4ad9db6d8bc76a8b0ec8ff2df617cfc8e3
pdiqui-17 <quy.pham17012005@hcmut.edu.vn>
2025-10-08 11:06:44 +0700

    Notification add

---
+++ Notification_add.sql
@@ -0,0 +1,11 @@
+INSERT INTO Notification (NoID, Content, SentDate) VALUES
+(1, N'Cảm ơn bạn đã quan tâm đến iPhone! Khám phá ngay các ưu đãi đặc biệt dành riêng cho bạn.', '2024-01-15 09:30:00'),
+(2, N'Đơn hàng iPhone 15 Pro của bạn đã được xác nhận và đang được chuẩn bị. Theo dõi đơn hàng tại đây.', '2024-01-16 14:20:00'),
+(3, N'iPhone 14 Pro Max đang có chương trình giảm giá 10% cho khách hàng thân thiết. Mua ngay kẻo lỡ!', '2024-01-17 11:00:00'),
+(4, N'3 đơn hàng iPhone của bạn đã được giao thành công. Cảm ơn bạn đã tin tưởng và ủng hộ chúng tôi!', '2024-01-18 16:45:00'),
+(5, N'Phụ kiện chính hãng Apple đang được giảm giá sốc khi mua kèm iPhone. Khám phá ngay!', '2024-01-19 10:15:00'),
+(6, N'Đơn hàng thứ 2 của bạn đang trên đường vận chuyển. Dự kiến giao hàng trong 24h tới.', '2024-01-20 13:30:00'),
+(7, N'Chào mừng bạn đến với cộng đồng người dùng iPhone! Nhận ngay voucher 500K cho lần mua tiếp theo.', '2024-01-21 15:20:00'),
+(8, N'5 đơn hàng iPhone của bạn đã được xử lý thành công. Cảm ơn bạn là khách hàng thân thiết của chúng tôi!', '2024-01-22 08:45:00'),
+(9, N'Bảo hành mở rộng Apple Care+ đang có ưu đãi đặc biệt cho người dùng iPhone mới.', '2024-01-23 12:10:00'),
+(10, N'Đơn hàng iPhone 15 của bạn đã sẵn sàng để giao. Vui lòng chuẩn bị số tiền [số tiền] để nhận hàng.', '2024-01-24 17:00:00');
