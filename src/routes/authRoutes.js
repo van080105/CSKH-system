0574f2143f766c1b72e64ce830105cb567c84469
van.ntc <van.nguyennhe2005@hcmut.edu.vn>
2025-11-17 09:45:36 +0700

    Add logout route with token verification

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { register, login, forgotPassword, changePassword} from "../controllers/authController.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 // POST /api/auth/register
 router.post("/register", register);
-
-// POST /api/auth/login
+// POST /api/auth/login-logout
 router.post("/login", login);
-
+router.post("/logout", verifyToken, logout);
 // POST /api/auth/forgot-password, change-password
 router.post("/forgot-password", forgotPassword);
 router.post("/change-password", changePassword); 
