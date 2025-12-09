25a963e342a4fba486128bd139d706b6a5097171
van.ntc <van.nguyennhe2005@hcmut.edu.vn>
2025-12-09 10:53:51 +0700

    Change status value in insert_form.sql

--- src/queries/insert_form.sql
+++ src/queries/insert_form.sql
@@ -1,6 +1,6 @@
 
 DECLARE @tableID INT;
-INSERT INTO Form (FormID, Title, Content, Stt, Typ, SentDate) values (@formID,@title,@content,'Pending',@type,GETDATE()) ;
+INSERT INTO Form (FormID, Title, Content, Stt, Typ, SentDate) values (@formID,@title,@content,N'Chưa trả lời',@type,GETDATE()) ;
 INSERT INTO CustomerCreate (CustomerID, FormID) values (@id,@formID);
 SELECT @tableID = TableID from ClassifyTable where NameTable = @type;
 INSERT INTO Classify (FormID,TableID) values (@formID, @tableID);
