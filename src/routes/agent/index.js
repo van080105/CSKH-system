58d8bd4d187e71f18635693b708902e2ecb8ca90
van.ntc <van.nguyennhe2005@hcmut.edu.vn>
2025-12-09 10:52:11 +0700

    Add PUT route to escalate form in agent routes

--- src/routes/agent/index.js
+++ src/routes/agent/index.js
@@ -7,5 +7,6 @@ import controller_form from '../../controllers/agent/agent_controller';
 
 import controller_order from '../../controllers/agent/agent_controller_order';
 router.get('/form', verifyToken, authorizeRole('Agent'), controller_form.get_form)
+router.put('/update_form',verifyToken,authorizeRole('Agent'),controller_form.escalate_form)
 router.put('/order/update/:orderId', verifyToken, authorizeRole('Agent'), controller_order.update_order)
 export default router;
